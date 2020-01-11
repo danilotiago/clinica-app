8f9a88fcb1beaee3fd2d49996c06cee7f3e1f9c4
Heitor Ferreira <heitor.ferreira@mokedsystem.com>
2020-01-11 12:48:36 -0300

    page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./pages/list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 
 @NgModule({
