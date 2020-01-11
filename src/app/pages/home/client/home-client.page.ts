21c1e38ca8ef2b49c6acb9d55360ed7f6451562a
danilotiago <programacao.desenvolvimento@gmail.com>
2020-01-11 08:25:16 -0300

    reordenando home de cliente

---
+++ src/app/pages/home/client/home-client.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-client',
+  templateUrl: 'home-client.page.html',
+  styleUrls: ['home-client.page.scss'],
+})
+export class HomeClientPage {
+
+  constructor() {}
+
+}
