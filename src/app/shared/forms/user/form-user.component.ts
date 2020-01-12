aecf0f13c5770a46700336fce744875a81140bb6
Heitor Ferreira <heitor.ferreira@mokedsystem.com>
2020-01-12 19:31:13 -0300

    ajustes de formulario

--- src/app/shared/forms/user/form-user.component.ts
+++ src/app/shared/forms/user/form-user.component.ts
@@ -11,4 +11,7 @@ export class FormUserComponent implements OnInit {
 
   ngOnInit() {}
 
+  showCities(){
+    console.log("teste")
+  }
 }
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { Menu } from './menu';
 import { menuList } from './menu-list';
 
 @Component({
-  selector: 'clinica-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: 'clinica-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
 
-  protected menuList: Menu[] = menuList;
+    protected menuList: Menu[] = menuList;
+    showMenu: boolean = true;
+    constructor(private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.showMenu = !event.url.includes("/login");
+            }
+        })
+    }
 
 }
